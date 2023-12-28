7d15839769d3fd76eb45c43a3278782c3d8362dc
hungle11 <hunglqph43918@fpt.edu.vn>
2023-12-28 22:45:28 +0700

    Desgin detail

---
+++ src/pages/Detail.vue
@@ -0,0 +1,155 @@
+<script lang="ts">
+
+</script>
+    
+<template>
+    <div class="fixed-top menu">
+        <div class="container d-flex justify-content-between align-items-center ">
+            <div>
+                <p class="fs-3 text-danger my-2 fw-bold">VILLA</p>
+            </div>
+            <div class="d-flex">
+                <a class="fw-bold text_menu" href="#">Home</a>
+                <a class="mx-3 fw-bold text_menu" href="#">Properties</a>
+                <a class="fw-bold text_menu" href="#">Property Details</a>
+                <a class="mx-3 fw-bold text_menu" href="#">Contact Us</a>
+
+            </div>
+        </div>
+
+    </div>
+
+    <div class="banner">
+        <div class="d-flex justify-content-center flex-row">
+            <p class="text-white my-4">HOME / SINGLE PROPERTY</p>
+        </div>
+        <h1 class="text-white text-center mb-5">SINGLE PROPERTY</h1>
+        <div class="d-flex justify-content-center flex-row">
+            <marquee behavior="scroll" direction="left" style="color: red; width: 400px;text-align: center;">
+                <h1>24 New Street Miami, OR 24560</h1>
+            </marquee>
+        </div>
+    </div>
+
+
+    <div class="container mt-5 d-flex justify-content-center gap-5">
+        <img class="" src="https://templatemo.com/templates/templatemo_591_villa_agency/assets/images/single-property.jpg"
+            alt="" height="412px">
+        <div class="content1_box3 w-25 h-50">
+            <div class="align-self-center mt-3">
+                <img class="img_icon"
+                    src="https://templatemo.com/templates/templatemo_591_villa_agency/assets/images/info-icon-01.png"
+                    alt="">
+                <label>
+                    <h3>250 m2</h3>
+                    <p>Total Flat Space</p>
+                </label>
+
+            </div>
+
+            <div>
+                <img class="img_icon"
+                    src="https://templatemo.com/templates/templatemo_591_villa_agency/assets/images/info-icon-02.png"
+                    alt="">
+                <label>
+                    <h3>Contract Ready</h3>
+                    <p>Contract Ready</p>
+                </label>
+
+            </div>
+
+            <div>
+                <img class="img_icon"
+                    src="https://templatemo.com/templates/templatemo_591_villa_agency/assets/images/info-icon-03.png"
+                    alt="">
+                <label>
+                    <h3>Payment</h3>
+                    <p>Payment Process</p>
+                </label>
+
+            </div>
+
+            <div>
+                <img class="img_icon"
+                    src="https://templatemo.com/templates/templatemo_591_villa_agency/assets/images/info-icon-04.png"
+                    alt="">
+                <label>
+                    <h3>Safety</h3>
+                    <p>24/7 Under Control</p>
+                </label>
+            </div>
+
+        </div>
+    </div>
+
+
+    <div class="container" style="margin-left: 190px; margin-top: 50px;">
+        <!-- category sản phẩm -->
+        <div
+            style="border: orangered 1px solid; border-radius: 5px; background-color: #fbd9cf; width: 100px; padding: 5px; margin-bottom: 20px;">
+            <p style="margin: 0px; padding: 0px;text-align: center;font-weight: 600;">Apparment</p>
+        </div>
+        <!-- tên sản phẩm -->
+        <div style="display: flex;margin-top: 50px;align-self: center;gap: 240px;">
+            <p style=" font-weight: 700; font-size: 20px;">24 New Street Miami, OR 24560</p>
+            <div
+                style="border: 1px solid orangered; border-radius: 8px;background-color:#fbd9cf;width: 200px;height: 30px;">
+                <p class="text-center m-0 p-0"><a href="" class="text-decoration-none text-black fw-bold">Add to cart</a>
+                </p>
+            </div>
+        </div>
+    </div>
+    <hr class="container">
+    <!-- chi tiết sản phẩm -->
+    <div class="container" style="margin-left: 190px; margin-top: 50px; width: 850px;">
+        <p style="font-weight: 600;">Get the best villa agency HTML CSS Bootstrap Template for your company website.
+            TemplateMo provides you the best free CSS templates in the world. Please tell your friends about it. Thank you.
+            Cloud bread kogi bitters pitchfork shoreditch tumblr yr succulents single-origin coffee schlitz enamel pin you
+            probably haven't heard of them ugh hella.
+            When you look for free CSS templates, you can simply type TemplateMo in any search engine website. In addition,
+            you can type TemplateMo Digital Marketing, TemplateMo Corporate Layouts, etc. Master cleanse +1 intelligentsia
+            swag post-ironic, slow-carb chambray knausgaard PBR&B DSA poutine neutra cardigan hoodie pop-up.</p>
+    </div>
+
+    <div style="background-color: black; color: white;padding: 15px;margin-top: 40px;">
+        <p class="container text-center">Copyright © 28/12/2023 Group TTS: LH.VH.MQ Design: LH</p>
+    </div>
+</template>
+
+<style scoped>
+.menu {
+    backdrop-filter: blur(10px);
+
+}
+
+.text_menu {
+    color: black;
+    text-decoration: none;
+}
+
+.text_menu:hover {
+    color: rgb(174, 26, 26);
+    border-bottom: 1px red solid;
+}
+
+.banner {
+    background-image: url("https://templatemo.com/templates/templatemo_591_villa_agency/assets/images/page-heading-bg.jpg");
+    background-size: cover;
+    height: 300px;
+    margin-top: 60px;
+}
+
+.content1_box3 {
+    box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
+    border: black solid 1px;
+    border-radius: 10px;
+    padding-left: 20px;
+    padding-right: 20px;
+
+}
+
+.img_icon {
+    margin-bottom: 35px;
+    margin-right: 5px;
+}
+</style>
